07f45a1248b09126c51d684cd9934736bc791593
eurcvf <eurcvf@hotmail.com>
2023-09-30 13:50:30 -0300

    Adicionado componente de post

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Header } from './components/Header/Header';
 import { Sidebar } from './components/Sidebar/Sidebar';
+import { Post } from './components/Post/Post';
+
 import './styles/global.scss';
 import styles from './styles/App.module.scss';
 
@@ -12,7 +14,7 @@ export function App() {
         <Sidebar />
 
         <main>
-          <span>Aqui fica os posts</span>
+          <Post />
         </main>
       </div>
     </>
---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,29 @@
+import styles from './Post.module.scss';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header className={styles.header}>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/122177641?v=4" />
+          <div className={styles.authorInfo}>
+            <strong>Roberto Filho</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time dateTime='2023-09-30 13:21:00' title='30 de setembro de 2023'>Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+        <p>👉 <a href="#">jane.design/doctorcare</a></p>
+
+        <p><a href="#">#novoprojeto</a> <a href="#">#nlw</a> <a href="#">#rocketseat</a></p>
+      </div>
+    </article>
+  )
+}
