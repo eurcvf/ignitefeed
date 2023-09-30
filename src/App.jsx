346043a5c4285fa6d4d5db2d033c828c46b505f8
eurcvf <eurcvf@hotmail.com>
2023-09-30 01:27:16 -0300

    Iniciando componente de Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { Header } from './components/Header/Header';
+import { Sidebar } from './components/Sidebar/Sidebar';
 import './styles/global.scss';
+import styles from './styles/App.module.scss';
 
 export function App() {
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <span>Aqui fica os posts</span>
+        </main>
+      </div>
     </>
   )
 }
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,23 @@
+import styles from './Sidebar.module.scss';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src="https://images.unsplash.com/photo-1519575706483-221027bfbb31?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" />
+
+      <div className={styles.profile}>
+        <img src="https://avatars.githubusercontent.com/u/122177641?v=4" className={styles.avatar} />
+
+        <strong>Roberto Filho</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer className={styles.footer}>
+        <a className={styles.footerEditProfile} href="#">
+          Editar meu perfil
+        </a>
+      </footer>
+
+    </aside>
+  )
+}
