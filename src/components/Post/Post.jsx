79b25721214003923ecca4ae29c6f08722e39e7b
eurcvf <eurcvf@hotmail.com>
2023-09-30 14:15:36 -0300

    Adicionado formulário de comentários

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -24,6 +24,16 @@ export function Post() {
 
         <p><a href="#">#novoprojeto</a> <a href="#">#nlw</a> <a href="#">#rocketseat</a></p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe sua avaliação</strong>
+
+        <textarea placeholder='Deixe um comentário' />
+
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
